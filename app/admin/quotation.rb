b11fd91e472b9170cd08bcb304a04d7f888d5e2a
Prelang Builder <builder@prelang.com>
2016-04-12 08:36:13 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Quotation' to allow for create, update

--- app/admin/quotation.rb
+++ app/admin/quotation.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Quotation do
+  permit_params :client_name, :client_email, :client_phone_number, :project_name, :website_url_to_develop, :business_name, :about_client_or_about_the_business, :website_platform, :user_name, :password, :logo, :website_color_scheme, :overall_ideas_for_the_project, :reference_websites_images_or_function, :special_functions, :list_of_pages
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
